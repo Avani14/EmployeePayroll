abea0cdf216034ba2d70eeac5078bd119d961896
Avani14 <trivediavani14@gmail.com>
2022-07-02 20:17:11 +0530

    First Commit

---
+++ src/com/bridgelabz/employeepayroll/EmployeePayrollDriver.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeepayroll;
+
+public class EmployeePayrollDriver {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Payroll system.");
+    }
+}
